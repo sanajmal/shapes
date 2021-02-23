45f6786c1678a12e5d1a83269fb6879fd680905b
sanajmal <sanasaidhali.mkd@gmail.com>
2021-02-23 15:23:48 +0530

    program completed

--- main.py
+++ main.py
@@ -11,62 +11,72 @@ while True:
     choice = input("SELECT YOUR CHOICE")
     if choice == '1':
         print("YOU CHOOSE PERIMETER")
-        print("-------------------------")
-        print(".....ENTER YOUR CHOICE....")
-        print("1.CIRCLE")
-        print("2.SQUARE")
-        print("3.RECTANGLE")
-        print("4.TRIANGLE")
-        print("5.EXIT")
-        print("--------------------------")
-        #con = input("...DO YOU WANT TO CONTINUE....")
-        #if con == "yes" or  con == "y" or con == "Y":
+        while True:
+            print("-------------------------")
+            print(".....ENTER YOUR CHOICE....")
+            print("1.CIRCLE")
+            print("2.SQUARE")
+            print("3.RECTANGLE")
+            print("4.TRIANGLE")
+            print("5.EXIT")
+            print("--------------------------")
+            # con = input("...DO YOU WANT TO CONTINUE....")
+            # if con == "yes" or  con == "y" or con == "Y":
 
-        ch = input("WHICH SHAPE DO YOU WANT")
-        if ch == '1':
-            print(" YOU CHOOSE CIRCLE")
-            r = float(input("Input Radius : "))
-            perimeter = round(2 * 3.14 * r, 2)
-            print("Perimeter of Circle: ", perimeter)
+            ch = input("WHICH SHAPE DO YOU WANT")
 
-        elif ch == '2':
-            print("YOU CHOOSE SQUARE")
-            s = int(input("Side : "))
-            perimeter = 4 * s
-            print("Perimeter of square : ", perimeter)
+            if ch == '1':
+                print(" YOU CHOOSE CIRCLE")
+                r = float(input("Input Radius : "))
+                perimeter = round(2 * 3.14 * r, 2)
+                print("Perimeter of Circle: ", perimeter)
 
-        elif ch == '3':
-            print("YOU CHOOSE RECTANGLE")
-            l = int(input("Length : "))
-            w = int(input("Width : "))
-            perimeter = 2 * (l + w)
-            print("Perimeter of Rectangle : ", perimeter)
 
-        elif ch == '4':
-            print("YOU CHOOSE TRIANGLE")
-            a = int(input("Side 1 : "))
-            b = int(input("Side 2 : "))
-            c = int(input("Side 3 : "))
-            perimeter = a + b + c
-            print("Perimeter of Triangle : ", perimeter)
 
-        elif ch == '5':
-            print("....YOU ARE EXITED.....")
-            break
+            elif ch == '2':
+                print("YOU CHOOSE SQUARE")
+                s = int(input("Side : "))
+                perimeter = 4 * s
+                print("Perimeter of square : ", perimeter)
+
+            elif ch == '3':
+                print("YOU CHOOSE RECTANGLE")
+                l = int(input("Length : "))
+                w = int(input("Width : "))
+                perimeter = 2 * (l + w)
+                print("Perimeter of Rectangle : ", perimeter)
+
+            elif ch == '4':
+                print("YOU CHOOSE TRIANGLE")
+                a = int(input("Side 1 : "))
+                b = int(input("Side 2 : "))
+                c = int(input("Side 3 : "))
+                perimeter = a + b + c
+                print("Perimeter of Triangle : ", perimeter)
+
+            elif ch == '5':
+                print("....YOU ARE EXITED.....")
+                break
+
+            c = input("DO YOU WANT TO CONTINUE..")
+            if c == "yes" or c == "y" or c == "Y":
+                continue
 
     elif choice == '2':
         print("you choose area")
-        print("-------------------------")
-        print(".....ENTER YOUR CHOICE....")
-        print("1.CIRCLE")
-        print("2.SQUARE")
-        print("3.RECTANGLE")
-        print("4.TRIANGLE")
-        print("5.EXIT")
-        print("--------------------------")
-        choice = input("WHICH SHAPE DO YOU WANT")
 
         while True:
+            print("-------------------------")
+            print(".....ENTER YOUR CHOICE....")
+            print("1.CIRCLE")
+            print("2.SQUARE")
+            print("3.RECTANGLE")
+            print("4.TRIANGLE")
+            print("5.EXIT")
+            print("--------------------------")
+            choice = input("WHICH SHAPE DO YOU WANT")
+
+           # while True:
             if choice == '1':
                 print(" YOU CHOOSE CIRCLE")
                 r = int(input("Enter circle's radius length: "))
@@ -74,8 +84,8 @@ while True:
                 circle_area = pi * r * r
                 print(f"The area of circle is {circle_area}.")
                 print("--------------------------------------")
-                #input("DO YOU WANT TO CONTINUE")
-                #continue
+                    # input("DO YOU WANT TO CONTINUE")
+                    # continue
 
             elif choice == '2':
                 print("YOU CHOOSE SQUARE")
@@ -105,6 +115,9 @@ while True:
                 print("....YOU ARE EXITED.....")
                 break
 
+            c = input("DO YOU WANT TO CONTINUE..")
+            if c == "yes" or c == "y" or c == "Y":
+                continue
 
     elif choice == '3':
         print("-----YOU ARE EXITED-----")
